5fb20a77097e78a510c5b9a65ac9ff3ccc043df4
GusanuAlin <105922765+GusanuAlin@users.noreply.github.com>
2024-07-19 16:46:30 +0200

    Documentation added.

--- Palindrome number/palindrome.py
+++ Palindrome number/palindrome.py
@@ -1,5 +1,13 @@
 class Solution:
     def is_palindrome_str_manipulation(self, x):
+        """This fuction checks if a integer x is a palindrome number.
+
+        :param x: number
+        :type x: int
+        
+        :return: yes/no
+        :rtype: bool
+        """
         x=str(x)
         reversed_x = x[::-1]
         if x == reversed_x:
@@ -8,12 +16,20 @@ class Solution:
 
 
     def is_palindrome_mathematical_reversion(self, x):
+        """This fuction checks if a integer x is a palindrome number.
+
+        :param x: number
+        :type x: int
+        
+        :return: yes/no
+        :rtype: bool
+        """
         y = 0
         z = x
         while z > 0:
             n = z % 10
             z = int(z / 10)
-            y = y*10+n
+            y = y * 10 + n
         if x == y:
             return True
         else:
@@ -24,10 +40,10 @@ class Solution:
 
 if __name__=="__main__":
     solution = Solution()
-    x=-111
+    x=121
     is_palindrome = solution.is_palindrome_str_manipulation(x)
     print(is_palindrome)
-    is_palindrome_int = solution.is_palindrome_reverting_integers(x)
+    is_palindrome_int = solution.is_palindrome_mathematical_reversion(x)
     print(is_palindrome_int)
 
 
